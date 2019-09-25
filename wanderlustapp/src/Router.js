ed4ecefbb2d3948f97bc1bedec5c7e43967ee2cb
Carlos <miklo.concepcion@gmail.com>
2019-09-24 21:03:28 -0500

    login/signup updated. Establishing useEffect and axios. Established Router.js file, linking everythingup and lighting this like a christmas tree.

--- wanderlustapp/src/Router.js
+++ wanderlustapp/src/Router.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowswerRouter as Router, Route } from "react-router-dom";
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import ExperiencesCard from './components/ExperiencesCard';
-import CTA from './components/CTA';
+// import React from "react";
+// import { BrowswerRouter as Router, Route } from "react-router-dom";
+// import SignIn from './components/SignIn';
+// import SignUp from './components/SignUp';
+// import ExperiencesCard from './components/ExperiencesCard';
+// import CTA from './components/CTA';
 
-export function Router() {
-    return (
-        <BrowserRouter>
-            <Route exact path='/' component={WelcomePage} />
-            <Route path='/experiences' component={ExperiencesCard} />
-            <Route path='/SignUp' component={SignUp} />
-            <Route path='/SignIn' component={SignIn} />
-        </BrowserRouter>
-    );
-}
+// export function Router() {
+//     return (
+//         <BrowserRouter>
+//             <Route exact path='/' component={WelcomePage} />
+//             <Route path='/experiences' component={ExperiencesCard} />
+//             <Route path='/SignUp' component={SignUp} />
+//             <Route path='/SignIn' component={SignIn} />
+//         </BrowserRouter>
+//     );
+// }
