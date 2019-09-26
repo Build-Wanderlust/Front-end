85a28ae68ffe27e7fefae30c00dd6512c1e598dc
Carlos <miklo.concepcion@gmail.com>
2019-09-26 16:37:15 -0500

    resolving merge issues

---
+++ wanderlustapp/src/components/navbar/searchbar.js
@@ -0,0 +1,21 @@
+
+// import React from 'react';
+// import SearchBar from 'material-ui-search-bar'
+//  search bar styling
+// // ...
+// render() {
+//   return(
+//     <SearchBar
+//       onChange={() => console.log('onChange')}
+//       onRequestSearch={() => console.log('onRequestSearch')}
+//       style={{
+//         margin: '0 auto',
+//         maxWidth: 800
+//       }}
+//     />
+//   )
+// }
+
+
+
+// 'https://www.npmjs.com/package/material-ui-search-bar'
